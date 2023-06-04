ef4f32242a7c19f663773a2987092b683c0b0d70
Venkat <venka7m@gmail.com>
2023-06-05 02:10:49 +0530

    route

--- conduit-backend/app.js
+++ conduit-backend/app.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 3000
 
 app.use(
   cors({
-    origin: 'http://localhost:3001',
+    origin: 'https://conduit-706z.onrender.com',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
